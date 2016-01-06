9f3dfa679e0f1c4e118bc4050d98f149e85f1797
esauOp <Esau>
2016-01-05 19:16:55 -0600

    Saving telegram username

--- lib/tele_notify/telegram_controller.rb
+++ lib/tele_notify/telegram_controller.rb
@@ -2,7 +2,7 @@ module TeleNotify
   module Controller
     def webhook
       if params[:message]
-        user = TelegramUser.create( { telegram_id: params[:message][:from][:id], first_name: params[:message][:from][:first_name] } )
+        user = TelegramUser.create( { telegram_id: params[:message][:from][:id], first_name: params[:message][:from][:first_name], username: params[:message][:from][:username] } )
         if user
           user.send_message("Enhorabuena tus notificaciones estan activadas.")
         end
