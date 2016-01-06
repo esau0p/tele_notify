ae88454f7e30d831a72bdbbbc7f8d25924ea7437
esauOp <monkeycoss@gmail.com>
2016-01-05 19:04:52 -0600

    Changing welcome message

--- lib/tele_notify/telegram_controller.rb
+++ lib/tele_notify/telegram_controller.rb
@@ -4,7 +4,7 @@ module TeleNotify
       if params[:message]
         user = TelegramUser.create( { telegram_id: params[:message][:from][:id], first_name: params[:message][:from][:first_name] } )
         if user
-          user.send_message("Notifications are now active. To cancel, stop this bot in Telegram.")
+          user.send_message("Enhorabuena tus notificaciones estan activadas.")
         end
         render :nothing => true, :status => :ok
       end
