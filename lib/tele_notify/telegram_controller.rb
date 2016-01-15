688fe4a689777969aaf0b135189bfce4280615cf
esauOp <Esau>
2016-01-15 12:22:07 -0600

    Missing end

--- lib/tele_notify/telegram_controller.rb
+++ lib/tele_notify/telegram_controller.rb
@@ -8,7 +8,7 @@ module TeleNotify
           user = TelegramUser.create( { telegram_id: params[:message][:from][:id], first_name: params[:message][:from][:first_name], username: params[:message][:from][:username] } )
 
           if user
-            if (user.username.length > 0
+            if user.username.length > 0
               user.send_message("Enhorabuena tus notificaciones estan activadas.")
             else
               user.send_message("Es necesario un @Username (Alias) para interactuar con tu cuenta, ajustalo y enviame nuevamente el mensaje /start.")
