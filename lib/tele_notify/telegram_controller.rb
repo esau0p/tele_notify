8cb0aa7aa470cee4116a3d5b4b27f4c39d47cb4e
esauOp <Esau>
2016-01-15 12:36:58 -0600

    Eval params

--- lib/tele_notify/telegram_controller.rb
+++ lib/tele_notify/telegram_controller.rb
@@ -8,7 +8,9 @@ module TeleNotify
           user = TelegramUser.create( { telegram_id: params[:message][:from][:id], first_name: params[:message][:from][:first_name], username: params[:message][:from][:username] } )
 
           if user
-            if (params[:message][:from][:username]).length > 0
+            name = params[:message][:from][:username]
+
+            if name.length > 0
               user.send_message("Enhorabuena tus notificaciones estan activadas.")
             else
               user.send_message("Es necesario un @Username (Alias) para interactuar con tu cuenta, ajustalo y enviame nuevamente el mensaje /start.")
